e00158b8e0ebfd6c98311c59b073c88646404beb
Sergio <sergiocraft987@hotmail.com>
2021-03-17 10:49:04 +0100

    Empiezo Eje2

---
+++ src/Eje2/direccion.java
@@ -0,0 +1,18 @@
+package Eje2;
+
+public class direccion {
+
+	String calle;
+	String ciudad;
+	String codigo_postal;
+	String pais;
+	
+	public direccion(String calle, String ciudad, String codigo_Postal, String pais) {
+		
+		this.calle=calle;
+		this.ciudad=ciudad;
+		this.codigo_postal=codigo_Postal;
+		this.pais=pais;
+	}
+	
+}
---
+++ src/Eje2/persona.java
@@ -0,0 +1,16 @@
+package Eje2;
+
+public class persona {
+
+	String nombre;
+	String apellidos;
+	String NIF;
+	String direccion;
+	
+	public persona(String nombre, String apellidos, String NIF, String direccion) {
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.NIF=NIF;
+		
+	}
+}
